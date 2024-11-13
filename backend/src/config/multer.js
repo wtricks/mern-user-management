ed16556a4e280f634fc65baad1f76bb1e38bbda9
wtricks <wtricks.ak@gmail.com>
2024-11-14 00:09:19 +0530

    checked 'upload' folder.

--- backend/src/config/multer.js
+++ backend/src/config/multer.js
@@ -1,8 +1,15 @@
 import multer from 'multer';
+import path from 'path';
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'uploads/');
+        // check if directory exists
+        const uploadDir = path.join(process.cwd(), '..', 'uploads');
+        if (!fs.existsSync(uploadDir)) {
+            fs.mkdirSync(uploadDir);
+        }
+
+        cb(null, uploadDir);
     },
     filename: function (req, file, cb) {
         cb(null, Date.now() + '-' + file.originalname);
