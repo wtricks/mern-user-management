6a21ce1c2df23c84c6dfc9a255cedf2cf02efdf8
wtricks <wtricks.ak@gmail.com>
2024-11-14 15:42:37 +0530

    render error fixed.

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -7,8 +7,8 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
+            // useNewUrlParser: true,
+            // useUnifiedTopology: true,
         });
 
         console.log(`MongoDB Connected: ${conn.connection.host}`);
